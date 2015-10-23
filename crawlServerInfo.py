580c2f1c20383029b07ea97b981efb7c4c084534
boliu <Bob.Liu@blackboard.com>
2015-10-23 16:48:17 +0800

    merge from Tony PR

--- crawlServerInfo.py
+++ crawlServerInfo.py
@@ -1,12 +1,29 @@
+#!/usr/bin/python
+
 import urllib2  
 import os
 import time
 import smtplib  
-import string
-from email.mime.text import MIMEText  
+import string 
+
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+
+
+# Preparation: Check the folders, create if not exist  
+workPath = os.path.abspath('.')
+qa1Folder = workPath + '/ultra-qa1/'
+stgFolder = workPath + '/stg/'
+demoFolder = workPath + '/demo/'
+
+folderList = [qa1Folder, stgFolder, demoFolder]
+
+for i in range(len(folderList)):
+    if not os.path.exists(folderList[i]):
+        os.mkdir(folderList[i])
 
 def getId(server):
-	servers = {'qa1':'https://ultra-qa1.int.bbpd.io/', 'stg':'https://saas-stg.int.bbpd.io/', 
+	servers = {'qa1':'https://ultra-qa.int.bbpd.io/', 'stg':'https://saas-stg.int.bbpd.io/', 
 	           'demo':'https://ultra-demo.int.bbpd.io/'}
 	baseURL = servers[server]
 	print(baseURL)
@@ -88,16 +105,10 @@ def saveId(server,dir):
 
 def sendMail():
 	data = ['', '', '']
-	data[0] = saveId('qa1','/Users/boliu/crawlServerInfo/ultra-qa1/')
-	data[1] = saveId('stg','/Users/boliu/crawlServerInfo/stg/')
-	data[2] = saveId('demo','/Users/boliu/crawlServerInfo/demo/')
+	data[0] = saveId('qa1',qa1Folder)
+	data[1] = saveId('stg',stgFolder)
+	data[2] = saveId('demo',demoFolder)
 	if data[0][5] or data[1][5] or data[2][5]:
-		sender = 'Bob.Liu@blackboard.com'  
-		receivers = ('Bob.Liu@blackboard.com')
-		# smtpserver = 'outlook.office365.com'  
-		username = 'Bob.Liu@blackboard.com'  
-		password = 'Fight*9'  
-		
 		date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 		alertFlag = ['','','','','','']
 		dateInfo = ['', '', '', '', '', '']
@@ -132,7 +143,7 @@ def sendMail():
             """%(data[2][0],data[2][2]) if data[2][0] !='' and data[2][2] !='' else """ """  
 		msgReport = """
             		<p text-align="left">
-						%s<br><br>%s<br><br>%s
+						%s<br>%s<br>%s
 					</p>
 					"""%(qa1Report, stgReport, demoReport)
 
@@ -192,8 +203,8 @@ def sendMail():
 					"""%(qa1Report, stgReport, demoReport)
 		dateSend = time.strftime('%Y.%m.%d  %H:%M:%S', time.localtime(time.time()))
 		msg = MIMEText("""
-			<p><center><h3>Time:  %s</h3></center></p><br>
-			<table width="1000" border="1" cellspacing="0" cellpadding="4">
+			<p><h3>Script Execute Time:  %s</h3></p><br>
+			<table border="1" cellspacing="0" cellpadding="5">
 			  <tr>
 				<th>server</th>
 				<th>commit id</th>
@@ -202,15 +213,20 @@ def sendMail():
 		     %s
 		     %s
              %s
-		    </table><br><br>%s<br>%s"""%(dateSend, qa1Info, saasInfo, demoInfo, msgReport, jiraFormat), "html", "utf-8")
+		    </table><br><br>%s<br>%s<br><br><br>
+		    Thanks,<br>
+		    Learn Shanghai QA Team
+		    """%(dateSend, qa1Info, saasInfo, demoInfo, msgReport, jiraFormat), "html", "utf-8")
 
-		msg['subject'] = 'Server information'
-		smtp = smtplib.SMTP()  
-		smtp.connect('outlook.office365.com','submission')
-		smtp.starttls()  
-		smtp.ehlo() 
-		smtp.login(username, password)  
-		smtp.sendmail(sender, receivers, msg.as_string())  
-		smtp.quit()  
+		fro = 'printer.shanghai@blackboard.com'
+		to = 'Tony.Feng@blackboard.com'
+		smtpserver = '10.75.106.10:25'
+        msg['From'] = fro
+        msg['Subject'] = 'Servers information'
+        msg['To'] = to
+
+        smtp = smtplib.SMTP(smtpserver)
+        smtp.sendmail(fro, to, msg.as_string())
+        smtp.close()
 
 sendMail()
